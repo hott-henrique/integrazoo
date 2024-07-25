852182cca56f83dc3f18cfcba5f34d0806945659
Henrique Hott <cs.henriquemchott@gmail.com>
2024-07-25 20:28:55 -0300

    DETAILS: HerdView using the new standard for error and delete unused view.

--- lib/dairy_cattle_subsystem/view/herd_view.dart
+++ lib/dairy_cattle_subsystem/view/herd_view.dart
@@ -22,52 +22,54 @@ class HerdView extends StatefulWidget {
 }
 
 class _HerdView extends State<HerdView> {
+  Exception? exception;
 
     @override
     Widget build(BuildContext context) {
-        return FutureBuilder<List<Bovine>?>(
-            future: widget.controller.bovineController.readHerd(),
-            builder: (context, AsyncSnapshot<List<Bovine>?> snapshot) {
-                if (snapshot.hasData) {
-                    if (snapshot.data == null) {
-                        /* TODO: Log event. */
-                        return Container(
-                            padding: const EdgeInsets.all(8),
-                            child: const Text(
-                                'Algo falhou, favor contatar a equipe do INTEGRAZOO.',
-                                textAlign: TextAlign.center,
-                            )
-                        );
-                    }
-
-                    final cattle = snapshot.data!;
-
-                    ListView body = ListView.separated(
-                        padding: const EdgeInsets.all(8),
-                        itemCount: cattle.length + 1,
-                        itemBuilder: (BuildContext context, int index) {
-                            if (index == 0) {
-                                return ListTile(leading: const Icon(Icons.add, weight: 200),
-                                        title: const Text("ADICIONAR ANIMAL"),
-                                        onTap: () => Navigator.of(context)
-                                        .push(MaterialPageRoute(builder: (context) => BovineCreateForm(controller: widget.controller)))
-                                        .then((value) => setState(() { }))
-                                    );
-                            }
-
-                            return BovineListTile(cattle: cattle[index - 1]);
-                        },
-                        separatorBuilder: (BuildContext context, int index) => const Divider(),
-                    );
-
-                    return IntegrazooBaseApp(body: Scrollbar(thickness: 15, thumbVisibility: true, child: body));
-
-                } else {
-                    return const CircularProgressIndicator();
-                }
-            }
+      if (exception != null) {
+        return AlertDialog(
+          title: const Text('Falha ao procurar animais.'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[ const Text('Algo falhou ao procurar por animais.'),
+                                  const Text('Por favor, contate a equipe INTEGRAZOO.'),
+                                  Text(exception.toString()) ],
+            ),
+          ),
+          actions: <Widget>[ TextButton(child: const Text('Fechar'), onPressed: () => setState(() => exception = null)) ],
         );
-        // List<Bovine> cattle = widget.controller.bovineController.readHerd();
+      }
+      return FutureBuilder<List<Bovine>?>(
+        future: widget.controller.bovineController.readHerd(),
+        builder: (context, AsyncSnapshot<List<Bovine>?> snapshot) {
+          if (snapshot.hasData) {
+            final cattle = snapshot.data!;
+
+            ListView body = ListView.separated(
+              padding: const EdgeInsets.all(8),
+              itemCount: cattle.length + 1,
+              itemBuilder: (BuildContext context, int index) {
+                if (index == 0) {
+                  return ListTile(
+                    leading: const Icon(Icons.add, weight: 200),
+                    title: const Text("ADICIONAR ANIMAL"),
+                    onTap: () => Navigator.of(context)
+                                          .push(MaterialPageRoute(builder: (context) => BovineCreateForm(controller: widget.controller)))
+                                          .then((value) => setState(() { }))
+                  );
+                }
+
+                return BovineListTile(cattle: cattle[index - 1]);
+              },
+              separatorBuilder: (BuildContext context, int index) => const Divider(),
+            );
+
+            return IntegrazooBaseApp(body: Scrollbar(thickness: 15, thumbVisibility: true, child: body));
+          } else {
+            return const CircularProgressIndicator();
+          }
+        }
+      );
     }
     /* Icons:
      * Cow <a href="https://www.flaticon.com/free-icons/cow" title="cow icons">Cow icons created by kerismaker - Flaticon</a>
