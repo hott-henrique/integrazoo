f011debb0f2d73172909320b63953f4d5ed16cc1
Arthur Piloto <arthurpiloto.github@gmail.com>
2024-11-12 18:15:26 -0300

    connecting test with persistence

--- test/database_test.dart
+++ test/database_test.dart
@@ -1,8 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integrazoo/model/bovine/bovine.dart';
+import 'package:integrazoo/persistence/bovine_persistence.dart';
+import 'package:integrazoo/persistence/database_connector.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:path/path.dart';
+// import 'package:path/path.dart';
 
-void main() {
+void main() async {
+  // Nos testes, não é possível criar diretamente uma conexão com o banco de dados do celular. Para testar as funções do banco, criamos um banco de dados em memória, que é momentâneo, serve apenas para rodar os comandos 'flutter test'.
   late Database database;
   
   // O método sqfliteFfiInit() inicializa o sqflite_common_ffi para que o banco de dados possa ser aberto corretamente no ambiente de testes. Essas duas linhas garantem que o sqflite usará a implementação baseada em FFI para abrir e manipular o banco de dados em vez da implementação nativa do Android/iOS, que não é compatível com os testes unitários.
@@ -16,41 +20,28 @@ void main() {
   
   tearDown é o contrário de setUp. É uma função executada após cada teste. Geralmente, ela é usada para limpar ou restaurar o ambiente de teste, fechando o banco de dados, por exemplo.
   */
-
-  int counter = 0;
   setUp(() async {
-    counter++;
-    // Cria um banco de dados temporário para os testes. O banco de dados criado segue o mesmo padrão do banco que será utilizado no aplicativo real.
-    final path = join(await getDatabasesPath(), 'test_database.db');
-    database = await openDatabase(path, onCreate: (db, version) async {
-      await db.execute("""
-          CREATE TABLE IF NOT EXISTS Bovine(
-            id INTEGER UNIQUE PRIMARY KEY,
-            name TEXT,
-            sex INTEGER /* 0: Male, 1: Female */
-          );
-        """);
-      },
-      version: 1,
-    );
+    database = await openDatabase(inMemoryDatabasePath);
+    await BovinePersistence.onDatabaseCreate(database, 1);
+    DatabaseConnector.db = database;
   });
 
-  // Após rodar o teste, a tabela Bovine é limpa para não ocasionar em erros após a execução de novos testes.
+  // Após rodar o teste, o database é fechado para não dar conflito com outros testes que podem vir a ser implementados.
   tearDown(() async {
-    await database.execute("""DELETE FROM Bovine""");
+    await database.close();
   });
 
-  // Essa função não mostra nenhum dado no terminal, ela retorna 1 caso o teste passe e -1 caso contrário.
-  test('Insere um bovino e verifica se ele foi inserido no banco de dados', () async {
-    final bovine = {
-      'name': 'Bovino $counter',
-      'sex': 0
-    };
-    await database.insert('Bovine', bovine);
-    final List<Map<String, dynamic>> result = await database.query('Bovine');
-
-    // De acordo com o que é colocado no 'expect' o teste retornará algo específico. Caso haja alguma informação que não seja coerente com os dados presentes no banco de dados o teste retornará um erro.
-    expect(result.length, 0);
-    expect(result[0]['name'], 'Bovino $counter');
-  });
+  test('Deve criar bovinos e ler a lista de bovinos', () async {
+      // Cria bovinos
+      await BovinePersistence.createBovine(Bovine(0, 'Bovino teste', Sex.male));
+      await BovinePersistence.createBovine(Bovine(1, 'Bovino teste', Sex.female));
+      await BovinePersistence.createBovine(Bovine(2, 'Bovino teste', Sex.male));
+      
+      // Lê o rebanho
+      final herd = await BovinePersistence.readHerd();
+      print('===== Bovinos no Banco de Dados =====');
+      for (var bovine in herd) {
+        print('ID: ${bovine.id}, Name: ${bovine.name}, Sex: ${bovine.sex == Sex.male ? 'Male' : 'Female'}');
+      }
+    });
 }
